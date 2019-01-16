9da9afd013baa11c33ca8b754328c0b70e50e4bb
Joan Esteva <joanme87@gmail.com>
2019-01-16 18:17:28 +0000

    change port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 const routes = require('./controllers')
+const PORT = process.env.PORT || 3001
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -13,6 +14,6 @@ app.use(function(req, res, next) {
 
 app.use('/', routes)
 
-app.listen(3001, () =>
+app.listen(PORT, () =>
   console.log('Server is running on http://localhost:3001'),
 )
